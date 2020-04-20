38984197237331a3001246630ef9598f1c2288a7
adithwip <adithwidyapradipta@gmail.com>
2020-04-20 08:39:27 +0700

    change ids name and create ui variables app.js

---
+++ mini-project-1/finish/app.js
@@ -0,0 +1,7 @@
+// Kumpulkan semua UI
+
+const form = document.querySelector("#todo-form");
+const taskInput = document.querySelector("#task-input");
+const filterInput = document.querySelector("#filter-input");
+const todoList = document.querySelector("#todo-list");
+const clearTodos = document.querySelector("#clear-todos");
